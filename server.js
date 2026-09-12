2461b8bf0e5feee5d6a20e252ad6254c51942ec2
valioeru <valentin.oeru@cashmachine.ro>
2026-09-12 06:59:29 +0300

    Inregistreaza modulul de pungi curier

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ require("./modules/warehouse").register(router);
 require("./modules/financiar").register(router);
 require("./modules/calculator").register(router);
 require("./modules/awb").register(router);
+require("./modules/pungi").register(router);
 require("./modules/backup").register(router);
 require("./modules/decont").register(router);
 require("./modules/configurari").register(router);
